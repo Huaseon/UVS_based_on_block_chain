5fac3ad92a4f97a64db2ddd14af36039a94b0c31
H.SEON <dsj34473@163.com>
2024-12-28 21:38:39 +0800

    init

---
+++ src/main.py
@@ -0,0 +1,12 @@
+'''
+@File     : main.py
+@Time     : 2024/12/28 15:59:20
+@Author   : H.SEON
+@Contact  : dsj34473@163.com
+'''
+
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_main.py
@@ -0,0 +1,7 @@
+'''
+@File     : test_main.py
+@Time     : 2024/12/28 16:00:30
+@Author   : H.SEON
+@Contact  : dsj34473@163.com
+'''
+
